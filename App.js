289179ad664c8d0f3bd005dd8b45f05dc29b5fc3
chadtheturtle <66935665+chadtheturtle@users.noreply.github.com>
2024-02-25 22:01:32 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+// App.js
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import AppNavigation from './Navigation.js'; // This should be your navigation setup
+
+export default function App() {
+  return (
+    <>
+      <AppNavigation />
+      <StatusBar style="auto" />
+    </>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
